7ba84b9a3ae587cd22827d57da731c7574d7ca1f
Miguel Ruiz <mig.ruiz@gmail.com>
2018-07-19 23:22:39 +0100

    using username

--- travis.sh
+++ travis.sh
@@ -8,5 +8,5 @@ else
 fi
 docker pull $HUBNAME || true;
 docker build  --cache-from $HUBNAME  -t $HUBNAME  .
-echo "$DOCKER_PASSWORD" | docker login -u "coco" --password-stdin
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin 
 docker push $HUBNAME 
