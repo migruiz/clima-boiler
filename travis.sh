e77401dd812e10456d928a0bfcf60dbcf827d3c3
Miguel Ruiz <mig.ruiz@gmail.com>
2018-07-19 22:48:15 +0100

    fixing script

--- travis.sh
+++ travis.sh
@@ -6,6 +6,6 @@ else
 	HUBNAME=migruiz/$PI_APP:$TRAVIS_TAG;
 fi
 docker pull $HUBNAME || true
-docker build -f Dockerfile_$PI_TYPE --cache-from $HUBNAME  -t $HUBNAME  . || travis_terminate 1
+docker build  --cache-from $HUBNAME  -t $HUBNAME  . || travis_terminate 1
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin || travis_terminate 1
 docker push $HUBNAME  || travis_terminate 1
