eb3fe85556480d538d81ab6cabdf311dcb4435da
Miguel Ruiz <mig.ruiz@gmail.com>
2018-07-19 22:55:55 +0100

    testing travis ter,omate

--- travis.sh
+++ travis.sh
@@ -5,7 +5,7 @@ if [[ -z "$TRAVIS_TAG" ]]; then
 else
 	HUBNAME=migruiz/$PI_APP:$TRAVIS_TAG;
 fi
-docker pull $HUBNAME || true
-docker build  --cache-from $HUBNAME  -t $HUBNAME  . || travis_terminate 1
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin || travis_terminate 1
-docker push $HUBNAME  || travis_terminate 1
+docker pull $HUBNAME || true;
+docker build  -f Dockerfile_www --cache-from $HUBNAME  -t $HUBNAME  . || travis_terminate 1;
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin || travis_terminate 1;
+docker push $HUBNAME  || travis_terminate 1;
