b02ab41e04aec6b6d2b83214c8b2e9c469866312
Miguel Ruiz <mig.ruiz@gmail.com>
2018-07-19 23:05:36 +0100

    new synaasx

--- travis.sh
+++ travis.sh
@@ -1,4 +1,5 @@
 #!/bin/bash  
+set -ev
 HUBNAME=""
 if [[ -z "$TRAVIS_TAG" ]]; then
    HUBNAME=migruiz/$PI_APP;
@@ -6,6 +7,6 @@ else
 	HUBNAME=migruiz/$PI_APP:$TRAVIS_TAG;
 fi
 docker pull $HUBNAME || true;
-docker build  -f Dockerfile_www --cache-from $HUBNAME  -t $HUBNAME  . || travis_terminate 1;
-echo "$DOCKER_PASSWORD" | docker login -u "coco" --password-stdin || travis_terminate 1;
-docker push $HUBNAME  || travis_terminate 1;
+docker build  -f Dockerfile_www --cache-from $HUBNAME  -t $HUBNAME  .
+echo "$DOCKER_PASSWORD" | docker login -u "coco" --password-stdin
+docker push $HUBNAME 
