e62dc9715c8b747d03700bbf5b61041a8f53726e
Miguel Ruiz <miguel@dpsystems.ie>
2018-07-24 18:06:21 +0100

    zwave mock

--- App/ZWaveMock.js
+++ App/ZWaveMock.js
@@ -1,21 +1,27 @@
 function ZWaveMock(){
-    this.requestNodeState= function (nodeId) {
-        action(nodeid, comclass, value);
+    this.requestNodeState = function (nodeId) {
+
+        
+        var node = valves[nodeId];
+        for (var instance in node) {
+            var state = node[instance];
+            valueChangedHandler(nodeId, 37, new { value: state, instance: instance, class_id: 37 });
+        }
     }
     var valves = {
         4: {
-            1: 0,
-            3: 0
+            1: 0,  //testValve
+            3: 0 //hotwater
         },
         5: {
-            1: 0,
-            3: 0
+            1: 0,  //upstaisvalve
+            3: 0  //downstairsvalve
         },
     };
     
     this.setValue = function (nodeId, commandId, instanceId, subId, state) {
         val[nodeId][instanceId] = state;
-        valueChangedHandler(nodeId, commandId, state);
+        valueChangedHandler(nodeId, 37, state);
     }
     var valueChangedHandler;
     this.on= function (command, action) {
