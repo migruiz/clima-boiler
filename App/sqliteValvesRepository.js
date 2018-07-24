fb1ca0feefbd0c5f9e2c0ea7c6ff7d26d072b901
Miguel Ruiz <miguel@dpsystems.ie>
2018-07-24 16:54:43 +0100

    using async fxs

--- App/sqliteValvesRepository.js
+++ App/sqliteValvesRepository.js
@@ -1,40 +1,29 @@
-var await = require('asyncawait/await');
-var async = require('asyncawait/async');
-var sqliteDb = require('./db-sqlite.js');
-exports.updateValveStateAsync = function (valveState) {
-    await(sqliteDb.database().operateDatabaseAsync(function (db) {
-        await(db.runAsync("REPLACE INTO Valves(valveCode,state,stateTimestamp,stateOnLastTimestamp,triggeredBy) values ($valveCode,$state,$stateTimestamp,$stateOnLastTimestamp,$triggeredBy)",
-            {
-                $valveCode: valveState.valveCode,
-                $state: valveState.state,
-                $stateTimestamp: valveState.stateTimestamp,
-                $stateOnLastTimestamp: valveState.stateOnLastTimestamp,
-                $triggeredBy: valveState.triggeredBy
-            }));
-    }));
+var zonesdb = require('./zonesDatabase');
+exports.updateValveStateAsync = async function (valveState) {
+    await zonesdb.instance().runAsync("REPLACE INTO Valves(valveCode,state,stateTimestamp,stateOnLastTimestamp,triggeredBy) values ($valveCode,$state,$stateTimestamp,$stateOnLastTimestamp,$triggeredBy)",
+        {
+            $valveCode: valveState.valveCode,
+            $state: valveState.state,
+            $stateTimestamp: valveState.stateTimestamp,
+            $stateOnLastTimestamp: valveState.stateOnLastTimestamp,
+            $triggeredBy: valveState.triggeredBy
+        });
 }
-exports.getZonesValvesConfig = function () {
-    var result = await(sqliteDb.database().operateDatabaseAsync(function (db) {
-        var valveData = await(db.allAsync("select zoneCode,IFNULL(zoneAutoRegulateEnabled,0) zoneAutoRegulateEnabled ,IFNULL(zoneMinimumTemperature,0) zoneMinimumTemperature from ZoneValvesSettings"));
-        return valveData;
-    }));
+exports.getZonesValvesConfig = async function () {
+    var result = await zonesdb.instance().db.allAsync("select zoneCode,IFNULL(zoneAutoRegulateEnabled,0) zoneAutoRegulateEnabled ,IFNULL(zoneMinimumTemperature,0) zoneMinimumTemperature from ZoneValvesSettings");
     return result;
 };
 exports.getValveStateAsync = function (valveCode) {
 
-    var result = await(sqliteDb.database().operateDatabaseAsync(function (db) {
-        var valveData = await(db.getAsync("select valveCode valveCode,state state, stateTimestamp stateTimestamp,stateOnLastTimestamp stateOnLastTimestamp,triggeredBy triggeredBy from Valves where valveCode=$valveCode",
-            {
-                $valveCode: valveCode
-            }));
-        return valveData;
-    }));
+    var result = await zonesdb.instance().db.getAsync("select valveCode valveCode,state state, stateTimestamp stateTimestamp,stateOnLastTimestamp stateOnLastTimestamp,triggeredBy triggeredBy from Valves where valveCode=$valveCode",
+        {
+            $valveCode: valveCode
+        });
     return result;
 
 }
-exports.getZoneValveConfigByZoneCodeAsync = function (zoneCode) {
-    var result = await(sqliteDb.database().operateDatabaseAsync(function (db) {
-        var valveData = await(db.getAsync(`
+exports.getZoneValveConfigByZoneCodeAsync = async  function (zoneCode) {
+    var result = await zonesdb.instance().db.getAsync(`
             select 
             zoneCode
             ,IFNULL(zoneAutoRegulateEnabled, 0) zoneAutoRegulateEnabled
@@ -44,39 +33,32 @@ exports.getZoneValveConfigByZoneCodeAsync = function (zoneCode) {
             ,IFNULL(boostEnabled, 0) boostEnabled
             from ZoneValvesSettings
             where zoneCode= $zoneCode`,
-            {
-                $zoneCode: zoneCode
-            }));
-        return valveData;
-    }));
+        {
+            $zoneCode: zoneCode
+        });
     return result;
 };
-exports.setZoneValveAutoRegulatedEnabled = function (zoneCode, enabled) {
-    await(sqliteDb.database().operateDatabaseAsync(function (db) {
-        await(db.runAsync("replace into ZoneValvesSettings(zoneCode,zoneAutoRegulateEnabled,zoneMinimumTemperature) values ($zoneCode,$zoneAutoRegulateEnabled,(select zoneMinimumTemperature from ZoneValvesSettings where zoneCode=$zoneCode))",
-            {
-                $zoneCode: zoneCode,
-                $zoneAutoRegulateEnabled: enabled
-            }));
-    }));
+exports.setZoneValveAutoRegulatedEnabled = async  function (zoneCode, enabled) {
+    await zonesdb.instance().runAsync("replace into ZoneValvesSettings(zoneCode,zoneAutoRegulateEnabled,zoneMinimumTemperature) values ($zoneCode,$zoneAutoRegulateEnabled,(select zoneMinimumTemperature from ZoneValvesSettings where zoneCode=$zoneCode))",
+        {
+            $zoneCode: zoneCode,
+            $zoneAutoRegulateEnabled: enabled
+        });
+
 }
-exports.setZoneValveinimumTemperature = function (zoneCode, zoneMinimumTemperature) {
-    await(sqliteDb.database().operateDatabaseAsync(function (db) {
-        await(db.runAsync("replace into ZoneValvesSettings(zoneCode,zoneAutoRegulateEnabled,zoneMinimumTemperature) values ($zoneCode,(select zoneAutoRegulateEnabled from ZoneValvesSettings where zoneCode=$zoneCode),$zoneMinimumTemperature)",
-            {
-                $zoneCode: zoneCode,
-                $zoneMinimumTemperature: zoneMinimumTemperature
-            }));
-    }));
+exports.setZoneValveinimumTemperature = async  function (zoneCode, zoneMinimumTemperature) {
+    await zonesdb.instance()..runAsync("replace into ZoneValvesSettings(zoneCode,zoneAutoRegulateEnabled,zoneMinimumTemperature) values ($zoneCode,(select zoneAutoRegulateEnabled from ZoneValvesSettings where zoneCode=$zoneCode),$zoneMinimumTemperature)",
+        {
+            $zoneCode: zoneCode,
+            $zoneMinimumTemperature: zoneMinimumTemperature
+        });
 }
 
-exports.setZoneValveBoostInfo = function (zoneCode, boostInfo) {
-    await(sqliteDb.database().operateDatabaseAsync(function (db) {
-        await(db.runAsync("update  ZoneValvesSettings set boostStartTime=$boostStartTime, boostTime=$boostTime,boostEnabled=1 where zoneCode=$zoneCode",
-            {
-                $zoneCode: zoneCode,
-                $boostStartTime: boostInfo.boostStartTime,
-                $boostTime:boostInfo.boostTime
-            }));
-    }));
+exports.setZoneValveBoostInfo = async  function (zoneCode, boostInfo) {
+    await zonesdb.instance().runAsync("update  ZoneValvesSettings set boostStartTime=$boostStartTime, boostTime=$boostTime,boostEnabled=1 where zoneCode=$zoneCode",
+        {
+            $zoneCode: zoneCode,
+            $boostStartTime: boostInfo.boostStartTime,
+            $boostTime: boostInfo.boostTime
+        });
 }
