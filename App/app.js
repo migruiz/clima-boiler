3dc9496b1c23bcaa42b634c2cf11d20ef664073b
miguel <mig.ruiz@gmail.com>
2018-12-05 16:19:01 +0000

    removing not neede fies

--- App/app.js
+++ App/app.js
@@ -1,17 +1,5 @@
 var mqtt = require('./mqttCluster.js');
 const BoilerValve=require('./BoilerValve')
-global.config = {
-    zwaveDriverPath: '/dev/ttyACM0',
-    dbPath: 'c:\\valves.sqlite',
-    intranetAMQPURI: process.env.TEMPQUEUEURL,
-    internetAMQPURI: 'amqp://ikuaqslf:B6blp8fWqyBE4Ya7NBXhhVEk1guTw-M8@impala.rmq.cloudamqp.com/ikuaqslf',
-    valves: {
-        upstairsValve: { nodeId: 5, instanceId: 1, code: 'upstairsValve' },
-        downstairsValve: { nodeId: 5, instanceId: 3, code: 'downstairsValve' },
-        testValve: { nodeId: 4, instanceId: 1, code: 'testValve' },
-        hotWaterValve: { nodeId: 4, instanceId: 3, code: 'hotWaterValve' }
-    }
-};
 global.zonesConfiguration= {
     masterroom: 'upstairs',   
     livingroom:  'downstairs',
@@ -24,10 +12,6 @@ global.boilerValves={
     upstairs:new BoilerValve('upstairs'),
     downstairs:new BoilerValve('downstairs')
 }
-
-
-var ZWaveMockMan = require('./ZWaveMock.js');
-
 global.mtqqLocalPath = "mqtt://localhost";
 (async function(){
     for (var key in global.zonesConfiguration) {
@@ -53,57 +37,6 @@ global.mtqqLocalPath = "mqtt://localhost";
     })
   }
 
-return;
-
-
-//var ZWave = require('./node_modules/openzwave-shared/lib/openzwave-shared.js');
-var os = require('os');
-var individualValvesManagerCreator = require('./individualValveManager.js');
-var zonesValvesManagerCreator = require('./ZonesManager/ZonesValvesManager.js');
-var individualValveOperationReceiver = require('./individualValveOperationReceiver.js');
-var znodeValuesListener = require('./ValvesValueChangesListener.js');
-var valvesStateRequestReceiver = require('./valvesStateRequestReceiver.js');
-var zonesValvesConfigurationReceiver = require('./zonesValvesConfigurationReceiver.js');
-var firebaseSyncReceiver = require('./firebaseSyncReceiver.js');
-
-
-
-
-
-
-
-//var zwave = new ZWave({ ConsoleOutput: false });
-var zwave = new ZWaveMockMan.ZWaveMock();
-
-
-zwave.on('scan complete', function () {
-    var individualValveManager = individualValvesManagerCreator.getInstance(zwave);
-
-    var zonesValvesManager = zonesValvesManagerCreator.getInstance(individualValveManager);
-
-    individualValveOperationReceiver.startMonitoring(individualValveManager);
-    valvesStateRequestReceiver.startMonitoring(individualValveManager);
-
-    var valvesValueChangesListener = znodeValuesListener.getInstance(individualValveManager);
-    zwave.on('value changed', valvesValueChangesListener.handleChange);
-
-    firebaseSyncReceiver.startMonitoring();
-
-    zonesValvesConfigurationReceiver.startMonitoring(zonesValvesManager);
-    zonesValvesManager.startMonitoring();
-});
-
-
-
-zwave.on('driver failed', function () {
-    console.log('failed to start driver');
-    zwave.disconnect();
-    process.exit();
-});
-
-zwave.connect(global.config.zwaveDriverPath);
 process.on('SIGINT', function () {
-    console.log('disconnecting...');
-    zwave.disconnect(global.config.zwaveDriverPath);
     process.exit();
 });
