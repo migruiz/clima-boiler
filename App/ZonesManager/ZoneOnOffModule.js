b50d5f13f0f165d4b0195e36e672567b7787f9bc
miguel <mig.ruiz@gmail.com>
2018-12-05 11:28:24 +0000

    reading from database

--- App/ZonesManager/ZoneOnOffModule.js
+++ App/ZonesManager/ZoneOnOffModule.js
@@ -1,4 +1,5 @@
 const ZoneModule=require('./ZoneModule.js');
+var zonesdb = require('../zonesDatabase');
 class ZoneOnOffModule extends ZoneModule {
     constructor(zoneCode) {
         super(zoneCode)
@@ -8,6 +9,14 @@ class ZoneOnOffModule extends ZoneModule {
 
     }
     async initAsync() {
+        var result = await zonesdb.instance().operate(db=>db.getAsync("select IFNULL(zoneAutoRegulateEnabled,0) zoneAutoRegulateEnabled  from ZoneValvesSettings where zoneCode=$zoneCode",{
+            $zoneCode: this.zoneCode
+        }));
+        if (result){
+            this.Monitored=result.zoneAutoRegulateEnabled
+            console.log(this.zoneCode)
+            console.log(this.Monitored)
+        }
     }
     updateCurrentTemperature(temperature){}
     getCallingForHeatPriority() {
